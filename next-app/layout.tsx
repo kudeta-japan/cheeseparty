0c038ffea2f4ec4139250ff15dabb8cf62fc1cb9
kudeta-japan <yuya.is.yuya@gmail.com>
2025-09-23 15:45:48 +0900

    Create layout.tsx

---
+++ next-app/layout.tsx
@@ -0,0 +1,20 @@
+// next-app/app/layout.tsx
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CHEESE WONDERLAND",
+  icons: { icon: "/favicon.png" },
+  openGraph: {
+    url: "https://kudeta-japan.github.io/cheeseparty/",
+    images: ["/img/ogp.jpg"],
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
